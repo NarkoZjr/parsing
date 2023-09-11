35d63d5a4e4ca9842f949862122492ef34d0b6d9
NarkoZjr <143081069+NarkoZjr@users.noreply.github.com>
2023-09-11 09:53:24 +0300

    Update parsing url with out suc.py
    the script is written for parsing sites without protection from parsers (for example - wikipedia)

--- parsing url with out suc.py
+++ parsing url with out suc.py
@@ -21,14 +21,13 @@ def get_html(url):
     else:
         print('Boo!')
     return res.text
-#
-html = get_html('https://ru.wikipedia.org/wiki/%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD,_%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80_%D0%98%D0%BB%D1%8C%D0%B8%D1%87')
+
+
+html = get_html('URL')
 # print(html)
 bs = BeautifulSoup(html, 'lxml')
 name_title = bs.find("span", class_='mw-page-title-main').get_text()
 print(name_title)
-
-
 
 # resp = requests.get(url)
 # print(resp)
